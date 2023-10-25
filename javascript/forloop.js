d1d78727342d7b3b358c033f8b966a39e54b0bab
VinhPhuc <pphuc1996@gmail.com>
2023-10-25 16:03:02 +0700

    heh ?

---
+++ javascript/forloop.js
@@ -0,0 +1,65 @@
+// //for vd1
+// for (var n = 0; n <= 10; n++) {
+//   console.log(n);
+// }
+
+// //for vd2
+// for (var n = 0; n <= 10; n++) {
+//   if (n % 2 != 0) {
+//     console.log(n);
+//   }
+// }
+
+// //for vd3
+// for (var n = 0; n <= 10; n++) {
+//   if (n < 5) {
+//     console.log(n);
+//   }
+// }
+
+// //for vd4
+// for (var n = 0; n <= 10; n++) {
+//   if (n == 6) {
+//     console.log(n);
+//   }
+// }
+
+// //for vd5
+// sum = 0;
+// for (var n = 0; n < 11; n++) {
+//   sum += n;
+// }
+// console.log(sum);
+
+// //for vd6
+// s = 0;
+// for (var n = 0; n < 11; n++) {
+//   s = 1 + 1 / n;
+//   console.log(s);
+// }
+// let arr = [10, 14, 51, 666, 999];
+// function max(arr) {
+//   let i;
+//   let max = arr[0];
+
+//   for (i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) max = arr[i];
+//   }
+
+//   return max;
+// }
+
+//
+//
+// document.write('max is: ' + max(arr));
+// function namChanLe(n) {
+//   n = 0;
+//   if (n % 2 == 0) {
+//     console.log('nam chan');
+//   } else {
+//     console.log(' nam le');
+//   }
+// }
+
+// let n = 2021;
+// namChanLe(n);
